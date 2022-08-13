7da69a8127c09e3af822bb0928c62e15b2656984
aglide100 <aglide100@gmail.com>
2022-08-13 10:54:22 +0900

    update docker-compose file

---
+++ cmd/consumer/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+
+	"github.com/Shopify/sarama"
+)
+
+var (
+	brokerList        = kingpin.Flag("brokerList", "List of brokers to connect").Default("like-a-junk.com:2181").Strings()
+	topic             = kingpin.Flag("topic", "Topic name").Default("important").String()
+	partition         = kingpin.Flag("partition", "Partition number").Default("0").String()
+	offsetType        = kingpin.Flag("offsetType", "Offset Type (OffsetNewest | OffsetOldest)").Default("-1").Int()
+	messageCountStart = kingpin.Flag("messageCountStart", "Message counter start from:").Int()
+)
+
+func main() {
+	kingpin.Parse()
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+	brokers := *brokerList
+	master, err := sarama.NewConsumer(brokers, config)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer func() {
+		if err := master.Close(); err != nil {
+			log.Panic(err)
+		}
+	}()
+	consumer, err := master.ConsumePartition(*topic, 0, sarama.OffsetOldest)
+	if err != nil {
+		log.Panic(err)
+	}
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+	doneCh := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case err := <-consumer.Errors():
+				log.Println(err)
+			case msg := <-consumer.Messages():
+				*messageCountStart++
+				log.Println("Received messages", string(msg.Key), string(msg.Value))
+			case <-signals:
+				log.Println("Interrupt is detected")
+				doneCh <- struct{}{}
+			}
+		}
+	}()
+	<-doneCh
+	log.Println("Processed", *messageCountStart, "messages")
+}
