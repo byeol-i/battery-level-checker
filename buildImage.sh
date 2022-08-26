1fd8c337d1881bfaff8c532a568884794e31addb
aglide100 <aglide100@gmail.com>
2022-08-26 16:12:33 +0900

    retypo wrong path

--- buildImage.sh
+++ buildImage.sh
@@ -2,5 +2,5 @@
 
 docker build -t apid -f ./cmd/apid/Dockerfile .
 docker build -t consumer -f ./cmd/consumer/Dockerfile .
-docker build -t primaryDB -f ./cmd/db/Dockerfile.primary .
-docker build -t replicaDB -f ./cmd/db/Dockerfile.replica .
+docker build -t primary-dB -f ./cmd/db/Dockerfile.primary .
+docker build -t replica-dB -f ./cmd/db/Dockerfile.replica .
