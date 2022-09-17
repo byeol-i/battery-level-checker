a6783515714d3d95f77967f0cbfad84cd295b646
aglide100 <aglide100@gmail.com>
2022-09-17 16:55:26 +0900

    modify secret file path

--- shell/stackUpdate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ..
-
-docker stack deploy -c <(docker-compose -f traefik.yml config) traefik
-docker stack deploy -c <(docker-compose -f app.yml config) app
-#docker stack deploy -c traefik.yml traefik
-
-cd shell
