ced41e9e0dc122c0f3ac3aa5f5b9c386dc15ade8
HeeJun Moon <aglide100@gmail.com>
2023-07-18 16:33:38 +0900

    add deploy shell

--- shell/deployApp.sh
+++ shell/deployApp.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 cd ..
 
-NETWORK_NAME="kafka-network"
-
-EXISTING_NETWORK=$(docker network ls --filter name=$NETWORK_NAME --format '{{.Name}}')
-
-if [ "$EXISTING_NETWORK" == "$NETWORK_NAME" ]; then
-    echo "Network '$NETWORK_NAME' already exists."
-else
-    echo "Network '$NETWORK_NAME' does not exist. Creating network..."
-
-    docker network create -d overlay --attachable $NETWORK_NAME
-
-    echo "Network '$NETWORK_NAME' created successfully."
-fi
-
-CliApiVersion=$(docker version -f '{{.Client.APIVersion}}')
-echo "Using api version" $CliApiVersion
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    find .env -type f -exec sed -i '' -e /^CLI_API_VERSION=/s/=.*/=$CliApiVersion/ {} \;
-else
-    find .env -type f -exec sed -i -e /^CLI_API_VERSION=/s/=.*/=$CliApiVersion/ {} \;
-fi
-
 docker secret create firebase-key ./conf/firebase/key.json
 
 docker stack deploy -c <(docker-compose -f db.yml config) battery
--- shell/deployKafka.sh
+++ shell/deployKafka.sh
@@ -2,28 +2,6 @@
 
 cd ..
 
-NETWORK_NAME="kafka-network"
-
-EXISTING_NETWORK=$(docker network ls --filter name=$NETWORK_NAME --format '{{.Name}}')
-
-if [ "$EXISTING_NETWORK" == "$NETWORK_NAME" ]; then
-    echo "Network '$NETWORK_NAME' already exists."
-else
-    echo "Network '$NETWORK_NAME' does not exist. Creating network..."
-
-    docker network create -d overlay --attachable $NETWORK_NAME
-
-    echo "Network '$NETWORK_NAME' created successfully."
-fi
-
-CliApiVersion=$(docker version -f '{{.Client.APIVersion}}')
-echo "Using api version" $CliApiVersion
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    find .env -type f -exec sed -i '' -e /^CLI_API_VERSION=/s/=.*/=$CliApiVersion/ {} \;
-else
-    find .env -type f -exec sed -i -e /^CLI_API_VERSION=/s/=.*/=$CliApiVersion/ {} \;
-fi
-
 docker stack deploy -c kafka.yml kafka
 
 cd shell
